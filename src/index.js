834db6359ffd316823e7f5e9dc23eda78bba6544
David Taylor <davidtaylor92037@gmail.com>
2021-11-23 20:06:31 -0800

    pushing theme changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({ config })
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
